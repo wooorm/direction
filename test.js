5c37837bd56845720209cb81f1cb13515df89e15
Titus Wormer <tituswormer@gmail.com>
2022-11-20 10:54:24 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
-import assert from 'node:assert'
-import fs from 'node:fs'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import util from 'node:util'
 import childProcess from 'node:child_process'
 import {PassThrough} from 'node:stream'
-import test from 'tape'
+import test from 'node:test'
 import {direction} from './index.js'
 
-/** @type {{[key: string]: unknown, version: string}} */
-const pkg = JSON.parse(String(fs.readFileSync('package.json')))
+const exec = util.promisify(childProcess.exec)
+
+/** @type {import('type-fest').PackageJson} */
+const pack = JSON.parse(String(await fs.readFile('package.json')))
 
 const fixtures = [
   {input: '0', output: 'neutral'},
@@ -32,14 +35,14 @@ const fixtures = [
   {input: 'بسيطة', output: 'rtl'}
 ]
 
-test('api', function (t) {
+test('api', function () {
   let index = -1
 
-  // @ts-ignore
-  t.equal(direction(), 'neutral', 'should classify nullish as `neutral`')
+  // @ts-expect-error: missing argument.
+  assert.equal(direction(), 'neutral', 'should classify nullish as `neutral`')
 
   while (++index < fixtures.length) {
-    t.equal(
+    assert.equal(
       direction(fixtures[index].input),
       fixtures[index].output,
       'should classify `' +
@@ -49,62 +52,53 @@ test('api', function (t) {
         '`'
     )
   }
-
-  t.end()
 })
 
-test('cli', function (t) {
-  const input = new PassThrough()
-
-  t.plan(7)
-
-  childProcess.exec('./cli.js abc', function (error, stdout, stderr) {
-    t.deepEqual([error, stderr, stdout], [null, '', 'ltr\n'], 'arguments')
-  })
-
-  childProcess.exec('./cli.js @', function (error, stdout, stderr) {
-    t.deepEqual([error, stderr, stdout], [null, '', 'neutral\n'], 'neutral')
-  })
-
-  const subprocess = childProcess.exec(
-    './cli.js',
-    function (error, stdout, stderr) {
-      t.deepEqual([error, stderr, stdout], [null, '', 'rtl\n'], 'stdin')
-    }
+test('cli', async function () {
+  assert.deepEqual(
+    await exec('./cli.js abc'),
+    {stdout: 'ltr\n', stderr: ''},
+    'arguments'
   )
 
-  assert(subprocess.stdin, 'expected stdin')
-  input.pipe(subprocess.stdin)
-  input.write('لة')
-  setImmediate(function () {
-    input.end('الجم')
+  assert.deepEqual(
+    await exec('./cli.js @'),
+    {stdout: 'neutral\n', stderr: ''},
+    'neutral'
+  )
+
+  await new Promise(function (resolve) {
+    const input = new PassThrough()
+    const subprocess = childProcess.exec(
+      './cli.js',
+      function (error, stdout, stderr) {
+        assert.deepEqual([error, stdout, stderr], [null, 'rtl\n', ''], 'stdin')
+        setImmediate(resolve)
+      }
+    )
+    assert(subprocess.stdin, 'expected stdin on `subprocess`')
+    input.pipe(subprocess.stdin)
+    input.write('لة')
+    setImmediate(function () {
+      input.end('الجم')
+    })
   })
 
-  help('-h')
-  help('--help')
+  const h = await exec('./cli.js -h')
+  assert.ok(/\sUsage: direction/.test(h.stdout), '-h')
 
-  /**
-   * @param {string} flag
-   */
-  function help(flag) {
-    childProcess.exec('./cli.js ' + flag, function (error, stdout, stderr) {
-      t.deepEqual(
-        [error, stderr, /\s+Usage: direction/.test(stdout)],
-        [null, '', true],
-        flag
-      )
-    })
-  }
+  const help = await exec('./cli.js --help')
+  assert.ok(/\sUsage: direction/.test(help.stdout), '-h')
 
-  version('-v')
-  version('--version')
+  assert.deepEqual(
+    await exec('./cli.js -v'),
+    {stdout: pack.version + '\n', stderr: ''},
+    '-v'
+  )
 
-  /**
-   * @param {string} flag
-   */
-  function version(flag) {
-    childProcess.exec('./cli.js ' + flag, function (error, stdout, stderr) {
-      t.deepEqual([error, stderr, stdout], [null, '', pkg.version + '\n'], flag)
-    })
-  }
+  assert.deepEqual(
+    await exec('./cli.js --version'),
+    {stdout: pack.version + '\n', stderr: ''},
+    '--version'
+  )
 })
