a2c737ffe4de6c6970ebef816416dced3892c809
Titus Wormer <tituswormer@gmail.com>
2024-10-25 13:12:16 +0200

    Rewrite project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import {direction} from 'direction'
-
 /* eslint-env browser */
 
-var $input = document.querySelector('textarea')
-var $output = document.querySelector('output')
+import {ok as assert} from 'devlop'
+import {direction} from 'direction'
 
+const $input = document.querySelector('textarea')
+const $output = document.querySelector('output')
+assert($input)
+assert($output)
+
+$output.before(' ')
 $input.addEventListener('input', oninputchange)
 
 oninputchange()
 
 function oninputchange() {
+  assert($input)
+  assert($output)
   $output.textContent = direction($input.value)
 }
